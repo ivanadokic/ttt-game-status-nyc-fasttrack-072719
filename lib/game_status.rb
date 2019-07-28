82da7426fffee7964187abdb993f9dede553ad1e
dependable-helper-6910 <ivana_pile@yahoo.com>
2019-07-28 18:49:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,9 +8,3 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 
-  if position == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination # return the win_combination indexes that won.
-  else
-    false
-  end
-end
