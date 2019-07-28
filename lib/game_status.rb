20f1942d137307d02f821e77ee7a9d765ee1d204
dependable-helper-6910 <ivana_pile@yahoo.com>
2019-07-28 18:37:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,6 +7,5 @@ end
 
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
- win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
+if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination
